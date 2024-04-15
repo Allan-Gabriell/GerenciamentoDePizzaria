4929e88b7354ef7afd3f7ce60089f473308c104a
LeonardoA.S.Sousa <leonardoassousati@gmail.com>
2024-04-15 16:08:48 -0300

    Feat: Changes in pedido.h

--- include/pedido.h
+++ include/pedido.h
@@ -3,18 +3,18 @@
 
 #include "../src/pizzas.c"
 
-
+//Redifinição da struct order para Order
 typedef struct order Order;
-typedef struct queueNode QueueNode;
-typedef struct queue Queue;
-
-Queue* createQueue();
-void enqueue(Queue *queue);
-Node *orderListPizza(Node * list, Pizza new);
-Node* searchPizzaOrderByFlavor(Node* head, char* flavor);
-void printQueue(Queue *queue);
-void printOrdertxt(Queue *queue);
-Queue *loadOrder();
-void dequeue(Queue *queue);
+// Função para fazer o pedido de uma pizza.
+void makeOrder();
+// Função para encontrar uma pizza em uma lista de pizzas. Recebe um nó para criação de um auxiliar, além de dois caracters especificando
+// Sabor e Tamanho.
+Node *findPizza(Node *list_Pizza, char *flavor, char *size);
+// Função auxiliar para calcular o total do pedido de uma pizza, ele recebe uma lista que adiciona todos os pedidos feitos.
+float calOrdertotal(Node *pizzasOrded);
+// Função auxiliar para salvar o pedido no arquivo pizzaria.txt.
+void saveOrder(Order order, Node *pizzasOrded, float totalOrder);
+// Função para remover o primeiro cliente de uma lista de pizzas já salvas em um arquivo.txt
+void removeOrder();
 
 #endif //PEDIDO
